728c3732b8d47d3c410f412249d8888570bb31b1
LazyGeek7 <92102215+LazyGeek7@users.noreply.github.com>
2021-10-11 10:51:39 +0200

    adds dummy comment-11

--- src/oct_demo/Hi.java
+++ src/oct_demo/Hi.java
@@ -4,7 +4,7 @@ public class Hi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//dummy comment
 		System.out.println("Hi");
 		System.out.println("Hi Swathi");
                 System.out.println("Hello");
